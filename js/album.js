55aeab7d4c178c1e7eae271796f6fe87e0735426
jayden-creat3 <jaydenkamau508@gmail.com>
2025-06-26 08:09:28 +0300

    singles

--- js/album.js
+++ js/album.js
@@ -89,7 +89,7 @@ const musicCatalog = {
             id: '8',
             title: 'We Love You Tecca',
             year: 2019,
-            cover: './images/images/d07a0fc2-030b-40a3-808b-491d31ca2098.jpeg',
+            cover: './images/d07a0fc2-030b-40a3-808b-491d31ca2098.jpeg',
             tracks: 18,
             platforms: {
                 spotify: 'https://open.spotify.com/album/1I6FpscJIfwf4T56VxGVsB?si=sVjPLoGwR56nkcxstN6EZA',
@@ -97,9 +97,385 @@ const musicCatalog = {
                 youtube: 'https://youtube.com/playlist?list=OLAK5uy_n-HO4olRWoYjs0xZK_84o_SILW-035UyE&si=zwL6w5qQ19f0HZAd'
             }
       }
-
+    ],
+    singles: [
+        {
+            id: '1',
+            title: 'OWA OWA',
+            year: 2019,
+            cover: './images/OWA OWA.jpeg',
+            platforms: {
+                spotify: 'https://open.spotify.com/album/4Yhp37rgKSp0FZwEdwW0S7?si=4vUxqmfzSi-HVYNfmboZtA',
+                apple: 'https://music.apple.com/us/album/owa-owa-single/1817013284',
+                youtube: 'https://youtu.be/hnbN2L_tkCE?si=6h4FLF50c3ooSNMt'
+            }
+        },
+        {
+          id: '2',
+          title: 'Dark Thoughts',
+          year: 2025,
+          cover: './images/Dark Thoughts.jpeg',
+          platforms: {
+            spotify: 'https://open.spotify.com/album/7BnNV4WZAG7rFqUeMHRdUP?si=F0LNDUXFTK2KHzP9unGgQA',
+            apple: 'https://music.apple.com/us/album/dark-thoughts-single/1802035598',
+            youtube: 'https://youtu.be/Xtq_A2NnHKQ?si=UlkyVjAZ6SAulURC'
+          }
+        },
+        {
+            id: '3',
+            title: 'Down With Me',
+            year: 2024,
+            cover: './images/Dopamine.jpeg',
+            platforms: {
+                spotify: 'https://open.spotify.com/album/0CLqdKIh14TmKqLZCs9dml?si=lGIOCLKmT_KqRSariwsVQA',
+                apple: 'https://music.apple.com/us/album/dopamine-single/1819101379',
+                youtube: 'https://youtu.be/1b2g4j6f7a8?si=6h4FLF50c3ooSNMt'
+            }
+        },
+        {
+          id: '4',
+          title: 'Dead or Alive',
+          year: 2023,
+          cover: './images/a8a6344b-940a-479c-829f-200d326befd4.jpeg',
+          platforms: {
+              spotify: 'https://open.spotify.com/album/0CLqdKIh14TmKqLZCs9dml?si=lGIOCLKmT_KqRSariwsVQA',
+              apple: 'https://music.apple.com/us/album/i-know-how-it-feels-single/1819101379',
+              youtube: 'https://youtu.be/1b2g4j6f7a8?si=6h4FLF50c3ooSNMt'
+          }
+        },
+        {
+            id: '5',
+            title: 'HVN ON EARTH',
+            year: 2023,
+            cover: './images/Falling.jpeg',
+            platforms: {
+                spotify: 'https://open.spotify.com/album/6uWva3kfeqqVIsNAJ2MmeL?si=Zg8SSCt3TB-IAF1uKvn8qg',
+                apple: 'https://music.apple.com/us/album/falling-single/1767063070',
+                youtube: 'https://youtu.be/1b2g4j6f7a8?si=6h4FLF50c3ooSNMt'
+            }
+        },
+        {
+          id: '6',
+          title: '500Ibs',
+          year: 2023,
+          cover: './images/I Know How It Feels Remix.jpeg',
+          platforms: {
+              spotify: 'https://open.spotify.com/album/0CLqdKIh14TmKqLZCs9dml?si=lGIOCLKmT_KqRSariwsVQA',
+              apple: 'https://music.apple.com/us/album/i-know-how-it-feels-remix-single/1819101379',
+              youtube: 'https://youtu.be/1b2g4j6f7a8?si=6h4FLF50c3ooSNMt'
+          }
+        },
+        {
+            id: '7',
+            title: 'Need Me',
+            year: 2023,
+            cover: './images/Falling Remix.jpeg',
+            platforms: {
+                spotify: 'https://open.spotify.com/album/6uWva3kfeqqVIsNAJ2MmeL?si=Zg8SSCt3TB-IAF1uKvn8qg',
+                apple: 'https://music.apple.com/us/album/falling-remix-single/1767063070',
+                youtube: 'https://youtu.be/1b2g4j6f7a8?si=6h4FLF50c3ooSNMt'
+            }
+        },
+        {
+            id: '8',
+            title: 'Poppin',
+            year: 2023,
+            cover: './images/Falling Acoustic.jpeg',
+            platforms: {
+                spotify: 'https://open.spotify.com/album/6uWva3kfeqqVIsNAJ2MmeL?si=Zg8SSCt3TB-IAF1uKvn8qg',
+                apple: 'https://music.apple.com/us/album/falling-acoustic-single/1767063070',
+                youtube: 'https://youtu.be/1b2g4j6f7a8?si=6h4FLF50c3ooSNMt'
+            }
+        },
+        {
+            id: '9',
+            title: 'Blessing',
+            year: 2024,
+            cover: './images/Falling Acoustic Remix.jpeg',
+            platforms: {
+                spotify: 'https://open.spotify.com/album/6uWva3kfeqqVIsNAJ2MmeL?si=Zg8SSCt3TB-IAF1uKvn8qg',
+                apple: 'https://music.apple.com/us/album/falling-acoustic-remix-single/1767063070',
+                youtube: 'https://youtu.be/1b2g4j6f7a8?si=6h4FLF50c3ooSNMt'
+            }
+        },
+        {
+            id: '10',
+            title: 'Treesha',
+            year: 2022,
+            cover: './images/Falling Acoustic Remix.jpeg',
+            platforms: {
+                spotify: 'https://open.spotify.com/album/6uWva3kfeqqVIsNAJ2MmeL?si=Zg8SSCt3TB-IAF1uKvn8qg',
+                apple: 'https://music.apple.com/us/album/falling-acoustic-remix-single/1767063070',
+                youtube: 'https://youtu.be/1b2g4j6f7a8?si=6h4FLF50c3ooSNMt'
+            }
+        },
+        {
+            id: '11',
+            title: 'BAD TIME',
+            year: 2024,
+            cover: './images/Falling Acoustic Remix.jpeg',
+            platforms: {
+                spotify: 'https://open.spotify.com/album/6uWva3kfeqqVIsNAJ2MmeL?si=Zg8SSCt3TB-IAF1uKvn8qg',
+                apple: 'https://music.apple.com/us/album/falling-acoustic-remix-single/1767063070',
+                youtube: 'https://youtu.be/1b2g4j6f7a8?si=6h4FLF50c3ooSNMt'
+            }
+        },
+        {
+            id: '12',
+            title: 'Faster',
+            year: 2022,
+            cover: './images/Falling Acoustic Remix.jpeg',
+            platforms: {
+                spotify: 'https://open.spotify.com/album/6uWva3kfeqqVIsNAJ2MmeL?si=Zg8SSCt3TB-IAF1uKvn8qg',
+                apple: 'https://music.apple.com/us/album/falling-acoustic-remix-single/1767063070',
+                youtube: 'https://youtu.be/1b2g4j6f7a8?si=6h4FLF50c3ooSNMt'
+            }
+        },
+        {
+            id: '13',
+            title: 'Let Love Go',
+            year: 2022,
+            cover: './images/Falling Acoustic Remix.jpeg',
+            platforms: {
+                spotify: 'https://open.spotify.com/album/6uWva3kfeqqVIsNAJ2MmeL?si=Zg8SSCt3TB-IAF1uKvn8qg',
+                apple: 'https://music.apple.com/us/album/falling-acoustic-remix-single/1767063070',
+                youtube: 'https://youtu.be/1b2g4j6f7a8?si=6h4FLF50c3ooSNMt'
+            }
+        },
+        {
+            id: '14',
+            title: 'Number 2 / Never Last',
+            year: 2024,
+            cover: './images/Falling Acoustic Remix.jpeg',
+            platforms: {
+                spotify: 'https://open.spotify.com/album/6uWva3kfeqqVIsNAJ2MmeL?si=Zg8SSCt3TB-IAF1uKvn8qg',
+                apple: 'https://music.apple.com/us/album/falling-acoustic-remix-single/1767063070',
+                youtube: 'https://youtu.be/1b2g4j6f7a8?si=6h4FLF50c3ooSNMt'
+            }
+        },
+        {
+          id: '15',
+          title: 'TASTE',
+          year: 2024,
+          cover: './images/Falling Acoustic Remix.jpeg',
+          platforms: {
+              spotify: 'https://open.spotify.com/album/6uWva3kfeqqVIsNAJ2MmeL?si=Zg8SSCt3TB-IAF1uKvn8qg',
+              apple: 'https://music.apple.com/us/album/falling-acoustic-remix-single/1767063070',
+              youtube: 'https://youtu.be/1b2g4j6f7a8?si=6h4FLF50c3ooSNMt'
+          }
+        },
+        {
+            id: '16',
+            title: 'Fallin',
+            year: 2022,
+            cover: './images/Falling Acoustic Remix.jpeg',
+            platforms: {
+                spotify: 'https://open.spotify.com/album/6uWva3kfeqqVIsNAJ2MmeL?si=Zg8SSCt3TB-IAF1uKvn8qg',
+                apple: 'https://music.apple.com/us/album/falling-acoustic-remix-single/1767063070',
+                youtube: 'https://youtu.be/1b2g4j6f7a8?si=6h4FLF50c3ooSNMt'
+            }
+        },
+        {
+            id: '17',
+            title: 'Prada',
+            year: 2021,
+            cover: './images/Falling Acoustic Remix.jpeg',
+            platforms: {
+                spotify: 'https://open.spotify.com/album/6uWva3kfeqqVIsNAJ2MmeL?si=Zg8SSCt3TB-IAF1uKvn8qg',
+                apple: 'https://music.apple.com/us/album/falling-acoustic-remix-single/1767063070',
+                youtube: 'https://youtu.be/1b2g4j6f7a8?si=6h4FLF50c3ooSNMt'
+            }
+        },
+        {
+          id: '18',
+          title: 'prada (Remix)',
+          year: 2021,
+          cover: './images/Falling Acoustic Remix.jpeg',
+          platforms: {
+              spotify: 'https://open.spotify.com/album/6uWva3kfeqqVIsNAJ2MmeL?si=Zg8SSCt3TB-IAF1uKvn8qg',
+              apple: 'https://music.apple.com/us/album/falling-acoustic-remix-single/1767063070',
+              youtube: 'https://youtu.be/1b2g4j6f7a8?si=6h4FLF50c3ooSNMt'
+          }
+        },
+        {
+            id: '19',
+            title: 'REPEAT IT',
+            year: 2021,
+            cover: './images/Falling Acoustic Remix.jpeg',
+            platforms: {
+                spotify: 'https://open.spotify.com/album/6uWva3kfeqqVIsNAJ2MmeL?si=Zg8SSCt3TB-IAF1uKvn8qg',
+                apple: 'https://music.apple.com/us/album/falling-acoustic-remix-single/1767063070',
+                youtube: 'https://youtu.be/1b2g4j6f7a8?si=6h4FLF50c3ooSNMt'
+            }
+        },
+        {
+            id: '20',
+            title: 'Come Through',
+            year: 2021,
+            cover: './images/Falling Acoustic Remix.jpeg',
+            platforms: {
+                spotify: 'https://open.spotify.com/album/6uWva3kfeqqVIsNAJ2MmeL?si=Zg8SSCt3TB-IAF1uKvn8qg',
+                apple: 'https://music.apple.com/us/album/falling-acoustic-remix-single/1767063070',
+                youtube: 'https://youtu.be/1b2g4j6f7a8?si=6h4FLF50c3ooSNMt'
+            }
+        },
+        {
+            id: '21',
+            title: 'Money On Me',
+            year: 2021,
+            cover: './images/Falling Acoustic Remix.jpeg',
+            platforms: {
+                spotify: 'https://open.spotify.com/album/6uWva3kfeqqVIsNAJ2MmeL?si=Zg8SSCt3TB-IAF1uKvn8qg',
+                apple: 'https://music.apple.com/us/album/falling-acoustic-remix-single/1767063070',
+                youtube: 'https://youtu.be/1b2g4j6f7a8?si=6h4FLF50c3ooSNMt'
+            }
+        },
+        {
+            id: '22',
+            title: 'Show Me Up',
+            year: 2021,
+            cover: './images/Falling Acoustic Remix.jpeg',
+            platforms: {
+                spotify: 'https://open.spotify.com/album/6uWva3kfeqqVIsNAJ2MmeL?si=Zg8SSCt3TB-IAF1uKvn8qg',
+                apple: 'https://music.apple.com/us/album/falling-acoustic-remix-single/1767063070',
+                youtube: 'https://youtu.be/1b2g4j6f7a8?si=6h4FLF50c3ooSNMt'
+            }
+        },
+        {
+            id: '23',
+            title: 'JETSKI',
+            year: 2021,
+            cover: './images/Falling Acoustic Remix.jpeg',
+            platforms: {
+                spotify: 'https://open.spotify.com/album/6uWva3kfeqqVIsNAJ2MmeL?si=Zg8SSCt3TB-IAF1uKvn8qg',
+                apple: 'https://music.apple.com/us/album/falling-acoustic-remix-single/1767063070',
+                youtube: 'https://youtu.be/1b2g4j6f7a8?si=6h4FLF50c3ooSNMt'
+            }
+        },
+        {
+            id: '24',
+            title: 'Never Left',
+            year: 2021,
+            cover: './images/Falling Acoustic Remix.jpeg',
+            platforms: {
+                spotify: 'https://open.spotify.com/album/6uWva3kfeqqVIsNAJ2MmeL?si=Zg8SSCt3TB-IAF1uKvn8qg',
+                apple: 'https://music.apple.com/us/album/falling-acoustic-remix-single/1767063070',
+                youtube: 'https://youtu.be/1b2g4j6f7a8?si=6h4FLF50c3ooSNMt'
+            }
+        },
+        {
+            id: '25',
+            title: 'Our Time',
+            year: 2020,
+            cover: './images/Falling Acoustic Remix.jpeg',
+            platforms: {
+                spotify: 'https://open.spotify.com/album/6uWva3kfeqqVIsNAJ2MmeL?si=Zg8SSCt3TB-IAF1uKvn8qg',
+                apple: 'https://music.apple.com/us/album/falling-acoustic-remix-single/1767063070',
+                youtube: 'https://youtu.be/1b2g4j6f7a8?si=6h4FLF50c3ooSNMt'
+            }
+        },
+        {
+            id: '26',
+            title: 'Royal Rumble',
+            year: 2020,
+            cover: './images/Falling Acoustic Remix.jpeg',
+            platforms: {
+                spotify: 'https://open.spotify.com/album/6uWva3kfeqqVIsNAJ2MmeL?si=Zg8SSCt3TB-IAF1uKvn8qg',
+                apple: 'https://music.apple.com/us/album/falling-acoustic-remix-single/1767063070',
+                youtube: 'https://youtu.be/1b2g4j6f7a8?si=6h4FLF50c3ooSNMt'
+            }
+        },
+        {
+            id: '27',
+            title: 'Out of Love',
+            year: 2020,
+            cover: './images/Falling Acoustic Remix.jpeg',
+            platforms: {
+                spotify: 'https://open.spotify.com/album/6uWva3kfeqqVIsNAJ2MmeL?si=Zg8SSCt3TB-IAF1uKvn8qg',
+                apple: 'https://music.apple.com/us/album/falling-acoustic-remix-single/1767063070',
+                youtube: 'https://youtu.be/1b2g4j6f7a8?si=6h4FLF50c3ooSNMt'
+            }
+        },
+        {
+            id: '28',
+            title: 'All Star',
+            year: 2020,
+            cover: './images/Falling Acoustic Remix.jpeg',
+            platforms: {
+                spotify: 'https://open.spotify.com/album/6uWva3kfeqqVIsNAJ2MmeL?si=Zg8SSCt3TB-IAF1uKvn8qg',
+                apple: 'https://music.apple.com/us/album/falling-acoustic-remix-single/1767063070',
+                youtube: 'https://youtu.be/1b2g4j6f7a8?si=6h4FLF50c3ooSNMt'
+            }
+        },
+        {
+          id: '29',
+          title: 'IDK',
+          year: 2020,
+          cover: './images/Falling Acoustic Remix.jpeg',
+          platforms: {
+              spotify: 'https://open.spotify.com/album/6uWva3kfeqqVIsNAJ2MmeL?si=Zg8SSCt3TB-IAF1uKvn8qg',
+              apple: 'https://music.apple.com/us/album/falling-acoustic-remix-single/1767063070',
+              youtube: 'https://youtu.be/1b2g4j6f7a8?si=6h4FLF50c3ooSNMt'
+          }
+        },
+        {
+            id: '30',
+            title: 'Somebody',
+            year: 2019,
+            cover: './images/Falling Acoustic Remix.jpeg',
+            platforms: {
+                spotify: 'https://open.spotify.com/album/6uWva3kfeqqVIsNAJ2MmeL?si=Zg8SSCt3TB-IAF1uKvn8qg',
+                apple: 'https://music.apple.com/us/album/falling-acoustic-remix-single/1767063070',
+                youtube: 'https://youtu.be/1b2g4j6f7a8?si=6h4FLF50c3ooSNMt'
+            }
+        },
+        {
+            id: '31',
+            title: 'Why U Look Mad',
+            year: 2019,
+            cover: './images/Falling Acoustic Remix.jpeg',
+            platforms: {
+                spotify: 'https://open.spotify.com/album/6uWva3kfeqqVIsNAJ2MmeL?si=Zg8SSCt3TB-IAF1uKvn8qg',
+                apple: 'https://music.apple.com/us/album/falling-acoustic-remix-single/1767063070',
+                youtube: 'https://youtu.be/1b2g4j6f7a8?si=6h4FLF50c3ooSNMt'
+            }
+        },
+        {
+            id: '32',
+            title: 'Glo Up',
+            year: 2019,
+            cover: './images/Falling Acoustic Remix.jpeg',
+            platforms: {
+                spotify: 'https://open.spotify.com/album/6uWva3kfeqqVIsNAJ2MmeL?si=Zg8SSCt3TB-IAF1uKvn8qg',
+                apple: 'https://music.apple.com/us/album/falling-acoustic-remix-single/1767063070',
+                youtube: 'https://youtu.be/1b2g4j6f7a8?si=6h4FLF50c3ooSNMt'
+            }
+        },
+        {
+          id: '33',
+          title: 'Ransom (Remix)',
+          year: 2019,
+          cover: './images/Falling Acoustic Remix.jpeg',
+          platforms: {
+              spotify: 'https://open.spotify.com/album/6uWva3kfeqqVIsNAJ2MmeL?si=Zg8SSCt3TB-IAF1uKvn8qg',
+              apple: 'https://music.apple.com/us/album/falling-acoustic-remix-single/1767063070',
+              youtube: 'https://youtu.be/1b2g4j6f7a8?si=6h4FLF50c3ooSNMt'
+          }
+        },
+        {
+            id: '34',
+            title: 'Ransom',
+            year: 2024,
+            cover: './images/Falling Acoustic Remix.jpeg',
+            platforms: {
+                spotify: 'https://open.spotify.com/album/6uWva3kfeqqVIsNAJ2MmeL?si=Zg8SSCt3TB-IAF1uKvn8qg',
+                apple: 'https://music.apple.com/us/album/falling-acoustic-remix-single/1767063070',
+                youtube: 'https://youtu.be/1b2g4j6f7a8?si=6h4FLF50c3ooSNMt'
+            }
+        }
     ]
 };
+  
 
 const renderAlbums = () => {
     const albumList = document.getElementById('album-list');
@@ -125,6 +501,34 @@ const renderAlbums = () => {
         albumList.appendChild(albumCard);
     });
 };
+
+const renderSingles = () => {
+    const singleList = document.getElementById('single-list');
+    if (!singleList) return;
+    singleList.innerHTML = ''; // Clear previous content if any
+    musicCatalog.singles.forEach(single => {
+        const singleCard = document.createElement('div');
+        singleCard.className = 'music-card';
+        singleCard.innerHTML = `
+            <div class="artwork">
+                <img src="${single.cover}" alt="${single.title}">
+            </div>
+            <div class="info">
+                <h3>${single.title}</h3>
+                <p>${single.year}</p>
+                <div class="platforms">
+                    <a href="${single.platforms.spotify}" target="_blank">Listen on Spotify</a>
+                    <a href="${single.platforms.apple}" target="_blank">Listen on Apple Music</a>
+                    <a href="${single.platforms.youtube}" target="_blank">Watch on YouTube</a>
+                </div>
+            </div>
+        `;
+        singleList.appendChild(singleCard);
+    });
+};
+
+// Call the function to render singles on page load
+document.addEventListener('DOMContentLoaded', renderSingles);
 
 // Call the function to render albums on page load
 document.addEventListener('DOMContentLoaded', renderAlbums);
